0c446bb35a4123bc72bc016e4dd65b3c0b397526
Pedro Fluxa R <pfluxa@metricarts.com>
2019-05-07 22:37:42 -0400

    added simulations scripts for CLASS

---
+++ paper_scripts/class_sims/old_scripts/cross_pol_test/run_test_with_rotation.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# coding: utf-8
+import sys
+
+import pisco
+from pisco.beam_analysis.utils import *
+from pisco.convolution.core import deproject_sky_for_feedhorn
+from pisco.mapping.core import *
+from pisco.pointing import *
+from pisco.pointing.core import *
+from pisco.tod import *
+
+import astropy.units as u
+from astropy.coordinates import EarthLocation
+
+import pandas
+import time
+import numpy
+import healpy
+import pylab
+
+tag = sys.argv[1]
+
+# Load maps from input
+maps = numpy.load( sys.argv[2] )
+map_nside = maps['nside'][()]
+I_map     = maps['I']
+Q_map     = maps['Q']
+U_map     = maps['U']
+V_map     = maps['V']
+
+#----------------------------------------------------------------------------------------------------------#
+# Setup focal plane with a single detector aligned with the boresight
+#----------------------------------------------------------------------------------------------------------#
+#----------------------------------------------------------------------------------------------------------#
+receiver = Receiver()
+receiver.initialize(  [0],  # uid of detector
+                      [0],  # dx of detector
+                      [0],  # dy of detector,
+                      [0],) # polarization angle of detector
+#----------------------------------------------------------------------------------------------------------#
+
+# create Gaussian beam of 1.0 degree FWHM
+beam_nside = 512
+fwhm       = 1.0
+beam_co    = make_gaussian_elliptical_beam( beam_nside, fwhm, fwhm  , theta=0.0 )
+
+# Setup cross-polar beam
+beam_cx    = make_gaussian_elliptical_beam( beam_nside, fwhm, fwhm*2, theta= 45 )
+beam_cx   -= make_gaussian_elliptical_beam( beam_nside, fwhm, fwhm*2, theta=-45 )
+beam_cx   /= beam_cx.max()
+beam_cx   *= 0.01
+fwhm       = 0.5
+
+healpy.gnomview( beam_co, rot=(0,90), sub=(1,2,1), xsize=300, title='co-polar beam pattern' )
+healpy.gnomview( beam_cx, rot=(0,90), sub=(1,2,2), xsize=300, title='co-polar beam pattern' )
+pylab.show()
+
+# Setup co and cross polar beams. Null test only use co-polar
+beam_co_x   = numpy.copy( beam_co )
+beam_cx_x   = numpy.copy( beam_cx )
+
+beam_co_y   = numpy.copy( beam_co )
+beam_cx_y   = numpy.copy( beam_cx )
+
+# Setup map projection matrices
+AtA = 0.0
+AtD = 0.0
+
+# Setup all sky scanning strategy
+sky_pixels = numpy.arange( healpy.nside2npix( map_nside ) )
+tht, ra    = healpy.pix2ang( map_nside, sky_pixels )
+# HEALPY returns co-latitude, so convert to declination
+dec        = numpy.pi/2.0 - tht
+# Set parallactic angle to be zero + scan_angle
+pa         = numpy.zeros_like( ra )
+
+# Run scanning. Make three passes at different parallactic angles of -45, 0 and 45 degrees.
+for scan_angle in numpy.linspace(-45,45,100):
+    
+    print scan_angle
+
+    data = deproject_sky_for_feedhorn( ra, dec, pa + numpy.radians( scan_angle ),
+                                      receiver.pol_angles[0],
+                                      (I_map,Q_map,U_map,V_map),
+                                      beam_nside, beam_co_x, beam_cx_x, beam_co_y, beam_cx_y,
+                                      gpu_dev=0, maxmem=1024 )
+
+    # Reshape stuff to fool PISCO to think it is processing a regular TOD
+    fra   =  ra.reshape( (1,-1) )
+    fdec  = dec.reshape( (1,-1) )
+    fpa   =  pa.reshape( (1,-1) )
+    
+    # Same for the detector data
+    fdata = data.reshape( (1,-1) )
+
+    ata, atd = update_matrices(
+                     fra, fdec, fpa + numpy.radians( scan_angle ),
+                     receiver.pol_angles,
+                     fdata,
+                     map_nside )
+    AtA += ata
+    AtD += atd
+    
+    # Save matrices
+    numpy.savez( './runs/matrices_%s.npz' % (tag), AtA=AtA, AtD=AtD, nside=map_nside )
+
+I,Q,U,W = matrices_to_maps( map_nside, AtA, AtD )
+
+I[ W==0 ] = healpy.UNSEEN
+Q[ W==0 ] = healpy.UNSEEN
+U[ W==0 ] = healpy.UNSEEN
+W[ W==0 ] = healpy.UNSEEN
+
+healpy.mollview( I , sub=(1,3,1) , title='I')
+healpy.mollview( Q , sub=(1,3,2) , title='Q')
+healpy.mollview( U , sub=(1,3,3) , title='U')
+
+pylab.show()
+#pylab.savefig( 'maps.pdf' )
---
+++ paper_scripts/class_sims/old_scripts/run_fp_sim_with_grasp_beams_gpickup.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python
+# coding: utf-8
+import array_split
+from array_split import array_split, shape_split
+
+from matplotlib import rc
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+## for Palatino and other serif fonts use:
+#rc('font',**{'family':'serif','serif':['Palatino']})
+rc('text', usetex=True)
+
+import sys
+
+import pisco
+from pisco.beam_analysis.utils import *
+from pisco.convolution.core import deproject_sky_for_feedhorn
+from pisco.mapping.core import *
+from pisco.pointing import *
+from pisco.pointing.core import *
+from pisco.tod import *
+
+import astropy.units as u
+from astropy.coordinates import EarthLocation
+
+import os
+
+import pandas
+import time
+import numpy
+from numpy import pi
+import healpy
+import pylab
+
+# Do this shit right with argparser
+import argparse
+parser = argparse.ArgumentParser(description='Simulate CLASS maps using provided pointing.')
+parser.add_argument( '-maps' , action='store', type=str, dest='maps', 
+                      help='Input map to deproject' )
+parser.add_argument( '-tag' , action='store', type=str, dest='tag',
+                      help='Name of the output. Output will be matrices_tag.npz' )
+parser.add_argument( '-pointing', action='store', type=str, dest='pointing',
+                      help='NPZ file with the pointing of the season. See generete_pointing.py for more details.' )
+parser.add_argument( '-array_data', action='store', type=str, dest='array_data',
+                      help='CSV file with the array specificiations.' )
+parser.add_argument( '-beams',  action='store', type=str, dest='beams_path',
+                      help='Path to detector beams in HEAPIX format, packed as npz files.')
+parser.add_argument( '-beam_par_file',  action='store', type=str, dest='beam_par_file',
+                      help='File with specifications of the beam parameters for each detector.')
+args = parser.parse_args()
+
+# Setup simulation tag
+tag = args.tag
+
+#----------------------------------------------------------------------------------------------------------#
+# Setup experiment location
+location = EarthLocation( lat= -22.959748*u.deg, lon=-67.787260*u.deg, height=5200*u.m )
+#----------------------------------------------------------------------------------------------------------#
+#----------------------------------------------------------------------------------------------------------#
+# Load maps from input
+maps = numpy.load( args.maps )
+map_nside = maps['nside'][()]
+I_map     = maps['I'] 
+Q_map     = maps['Q'] 
+U_map     = maps['U']
+V_map     = maps['V']*0
+#----------------------------------------------------------------------------------------------------------#
+
+#----------------------------------------------------------------------------------------------------------#
+# Read in focal plane
+#----------------------------------------------------------------------------------------------------------#
+focalPlane = pandas.read_csv( args.array_data )
+receiver = Receiver()
+receiver.initialize(  focalPlane['uid'],
+                      focalPlane['az_off'],
+                      focalPlane['el_off'],
+                      numpy.radians( focalPlane['rot'] ) )
+#----------------------------------------------------------------------------------------------------------#
+
+#----------------------------------------------------------------------------------------------------------#
+# Read in pointing stream
+#----------------------------------------------------------------------------------------------------------#
+print 'loading pointing'
+pointing = numpy.load( args.pointing , mmap_mode='r')
+feed_ra  = pointing['ra']
+feed_dec = pointing['dec']
+feed_pa  = pointing['pa']
+inv_dets = pointing['invalid_dets']
+#----------------------------------------------------------------------------------------------------------#
+
+#----------------------------------------------------------------------------------------------------------#
+# Read beam parameter file
+print 'reading beam parameters'
+beam_data = pandas.read_csv( args.beam_par_file )
+uids      = beam_data[ 'uid']
+feeds     = beam_data[ 'feed']
+fwhm_x    = beam_data[ 'fwhm_x']
+fwhm_y    = beam_data[ 'fwhm_y']
+rotation  = beam_data[ 'rot']
+#----------------------------------------------------------------------------------------------------------#
+
+#----------------------------------------------------------------------------------------------------------#
+# Assemble beams in the focal plane
+print 'assembling focal plane beams'
+beams      = [None] * receiver.ndets
+for uid in uids:
+    
+    print 'loading', os.path.join( args.beams_path ,'detector%d.npz' % (uid) )
+    data = numpy.load( os.path.join( args.beams_path ,'detector%d.npz' % (uid) ) )
+    beam_nside = data['nside'][()]
+    
+    E_co, E_cx = data['E_co'], data['E_cx']
+    
+    beam_co   = E_co
+    beam_cx   = E_cx
+    
+    print beam_co.shape
+
+    '''
+    healpy.orthview( 20*numpy.log10(beam_co+0.00001),
+        rot=(0,90),
+        title='GRASP AZEL to HPX',
+        sub=(1,2,1),
+        min=-40, max=40,
+        half_sky=True,
+         )
+    healpy.orthview( 20*numpy.log10(beam_cx+0.00001),
+        rot=(0,90),
+        title='GRASP AZEL to HPX',
+        sub=(1,2,2),
+        min=-40, max=0,
+        half_sky=True,
+         )
+    
+    healpy.graticule( 10,10,c='red',alpha=0.5 )
+    pylab.show() 
+    '''
+    beams[uid] = { 'co': beam_co , 'cx': beam_cx, 'nside': beam_nside }
+#----------------------------------------------------------------------------------------------------------#
+
+#----------------------------------------------------------------------------------------------------------#
+# Setup data mask to avoid projecting invalid detectors
+#----------------------------------------------------------------------------------------------------------#
+data_mask = numpy.zeros( feed_ra.shape , dtype='int32' )
+for invalid_detector in inv_dets: 
+    data_mask[ invalid_detector ] += 1
+#----------------------------------------------------------------------------------------------------------#
+
+#----------------------------------------------------------------------------------------------------------#
+# Setup detector data buffer
+#----------------------------------------------------------------------------------------------------------#
+detector_data = numpy.zeros_like( feed_ra )
+#----------------------------------------------------------------------------------------------------------#
+
+#----------------------------------------------------------------------------------------------------------#
+# Read beam parameter file
+print 'reading beam parameters'
+beam_data = pandas.read_csv( args.beam_par_file )
+uids      = beam_data[ 'uid']
+feeds     = beam_data[ 'feed']
+fwhm_x    = beam_data[ 'fwhm_x']
+fwhm_y    = beam_data[ 'fwhm_y']
+rotation  = beam_data[ 'rot']
+#----------------------------------------------------------------------------------------------------------#
+
+# Commence deprojection procedure!!
+for det in receiver.uid:
+   
+    if det in inv_dets:
+        print 'detector %d was flagged as invalid. Skipping.' % (det)
+        continue
+    
+    # Find companion detector in the feedhorn
+    det_feed = (int)( focalPlane['feed'][ focalPlane['uid'] == det ] )
+    # Iterate over feedhorn until the matching detector is found
+    for uid in receiver.uid:
+        
+        if focalPlane['feed'][ uid ] == det_feed and uid != det:
+            pair_det = uid
+            break
+            
+    print 'deprojecting detector %d' % (det)
+    data = deproject_sky_for_feedhorn( 
+                  feed_ra[ det ], feed_dec[ det ], feed_pa[ det ],
+                  receiver.pol_angles[ det ],
+                  (I_map,Q_map,U_map,V_map),
+                  beams[ det ]['nside'],
+                  numpy.copy(beams[ det      ]['co']), 
+                  numpy.copy(beams[ det      ]['cx']), 
+                  numpy.copy(beams[ pair_det ]['co']), 
+                  numpy.copy(beams[ pair_det ]['cx']),
+                  gpu_dev=0, maxmem=7000, grid_size=1.0 )
+     
+    detector_data[ det ] = data
+    
+    pylab.plot( data , alpha=0.5 )
+    pylab.show()
+
+
+
+
+
+
+
+
